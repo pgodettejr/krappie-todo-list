f3db0076d2d8b94ad73cf6f3e6851e4968267105
pgodettejr <paul.godette@gmail.com>
2024-04-12 09:48:43 -0400

    Add and import CSS and image loaders to project
    Added necessary CSS and image loaders to the project via Webpack.
    Imported the CSS into the main JS file (index.js).

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,19 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Why isn't the image rule below a dev dependency for Webpack in the package.json file?
+      // REMINDER: there was an error regarding absolute file path (the internet link) and relative file path (local file in folder) before. Check TOP Discord previous comments
+      // The error was a "Not Found" IIRC. Webpack would find the image in the local project and send it to dist, but image showed up broken on the actual website.
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
