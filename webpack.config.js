f3909324623ea286ff076c3eb8ffd51d200164fc
pgodettejr <paul.godette@gmail.com>
2024-04-12 12:04:18 -0400

    Add HtmlWebpackPlugin to project along with setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Krappie To-Do List',
+      template: './src/index.html',
+      inject: 'body',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
