cb7e8a0792aa0d5dcc5f671e64c52917818550c1
pgodettejr <paul.godette@gmail.com>
2024-04-16 16:10:33 -0400

    Add code for appending logo via JS as comments

--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ import './sounds/quick-fart-with-reverb.mp3';
 // import Poop from './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
 // import reverbFart from './sounds/quick-fart-with-reverb.mp3';
 
+// Add logo to existing div. Not sure if this should go in the function below, it's own separate function or stay here by itself
+
+// const poopEmoji = new Image();
+// poopEmoji.src = Poop;
+
+// poopLogo.appendChild(poopEmoji);
+
 // Function for playing fart sound when user interacts with app logo. Click is enabled, but how can we also enable other interactions for accessibility like 'keydown' etc?
 // TODO: Uncaught Runtime error of 'The element has no supported sources'. Check src link of audio element in HTML
 // TODO: Need to find a Loader for audio sounds to add to the Webpack config with CSS and image files
 function poopSound() {
-  const poopLogo = document.querySelector("div[data-image=app-logo]");
+  const poopLogo = document.querySelector("div[data-image=app-logo]"); // Does this need to be outside this function if we implement logo add above?
 
   poopLogo.addEventListener('click', function(e) {
     const audio = document.querySelector("audio[data-image=app-logo]");
