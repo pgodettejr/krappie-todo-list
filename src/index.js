dae57acf8d5b6fc25f21ec9818093e288fe4a910
pgodettejr <paul.godette@gmail.com>
2024-04-16 13:02:41 -0400

    Import audio and image files to JS
    Imported the audio and image files to set up relative file path from the
    HTML document correctly. Edited the src links in the img elements in
    the HTML thinking that was the issue, but the fact that the files
    themselves weren't being imported to the JS was the actual issue. Made
    some additional comments regarding alternate ways to import the files
    and next steps resolving errors and finding an audio/sound loader for
    webpack. Moved the audio file to it's own sounds folder to resolve a
    separate error in DevTools.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './styles.css';
+import './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
+import './sounds/quick-fart-with-reverb.mp3';
+
+// Different method of importing the single files above
+
+// import Poop from './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
+// import reverbFart from './sounds/quick-fart-with-reverb.mp3';
 
 // Function for playing fart sound when user interacts with app logo. Click is enabled, but how can we also enable other interactions for accessibility like 'keydown' etc?
+// TODO: Uncaught Runtime error of 'The element has no supported sources'. Check src link of audio element in HTML
+// TODO: Need to find a Loader for audio sounds to add to the Webpack config with CSS and image files
 function poopSound() {
   const poopLogo = document.querySelector("div[data-image=app-logo]");
 
