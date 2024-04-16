80e69052330dfd5427378b7e6ddef008c73d727b
pgodettejr <paul.godette@gmail.com>
2024-04-16 11:45:10 -0400

    Add function to play audio on logo click
    Wrote the function to play the fart audio whenever the user clicks the
    app logo next to the title header on the page. Wrote a comment for
    implementing other ways to interact with the logo for accessibility
    purposes (keydown, etc). Updated comments in the CSS for potential
    logo sizing in the future (CSS selector change). Wrapped the logo image
    HTML element inside it's own div to target it better in the CSS and JS.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './styles.css';
 
-// Will need a button for the clickable logo that plays the audio (addEventListener). This file is likely the best place to implement it.
+// Function for playing fart sound when user interacts with app logo. Click is enabled, but how can we also enable other interactions for accessibility like 'keydown' etc?
+function poopSound() {
+  const poopLogo = document.querySelector("div[data-image=app-logo]");
+
+  poopLogo.addEventListener('click', function(e) {
+    const audio = document.querySelector("audio[data-image=app-logo]");
+    if(!audio) return; // stops function from running altogether
+    audio.currentTime = 0; // rewinds sound to the start
+    audio.play();
+  });
+};
+
+poopSound();
