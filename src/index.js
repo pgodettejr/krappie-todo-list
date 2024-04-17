a520c93de23744814bfcb44a22b77acbb43a9ca9
pgodettejr <paul.godette@gmail.com>
2024-04-17 11:59:10 -0400

    Remove comments no longer needed
    Removed all TODO comments regarding next steps as they have been
    completed up to this point. Removed commented out audio element in HTML
    as JS now generates the element dynamically. Edited the reset timer for
    the audio playback on the logo if a user multiclicks. Now resets to the
    1 second mark which is about where the audio actually starts (much
    closer to what we want). Still an initial delay on the first click
    however (not sure how to solve this other than get another file).
    
    Basically the JS and HTML files are cleaned up of unnecessary comments.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
 import './styles.css';
-// import './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
-// import './sounds/quick-fart-with-reverb.mp3';
-
-// Different method of importing the single files above
-
-// import Poop from './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
 import reverbFart from './sounds/quick-fart-with-reverb.mp3';
 
-// Add logo to existing div. Not sure if this should go in the function below, it's own separate function or stay here by itself
-
-// const poopEmoji = new Image();
-// poopEmoji.src = Poop;
-
-// poopLogo.appendChild(poopEmoji);
-
 // Function for playing fart sound when user interacts with app logo. Click is enabled, but how can we also enable other interactions for accessibility like 'keydown' etc?
-// TODO: Uncaught Runtime error of 'The element has no supported sources'. Check src link of audio element in HTML
-// TODO: Need to find a Loader for audio sounds to add to the Webpack config with CSS and image files
 function poopSound() {
-  const poopLogo = document.querySelector(".app-logo"); // Does this need to be outside this function if we implement logo add above?
+  const poopLogo = document.querySelector(".app-logo");
 
   poopLogo.addEventListener('click', function(e) {
     // Create the audio element via JS
@@ -37,7 +22,7 @@ function poopSound() {
     imageButton.appendChild(audio);
 
     if(!audio) return; // stops function from running altogether
-    audio.currentTime = 0; // rewinds sound to the start. Can I make this a 1 or 2 to get it to start later?
+    audio.currentTime = 1; // rewinds file to just before the sound clip starts (1 second mark)
     audio.play();
   });
 };
