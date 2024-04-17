f8255ad8d9ebcc925955bc8bd27d6b205b5e51a3
pgodettejr <paul.godette@gmail.com>
2024-04-17 11:51:04 -0400

    Modify audio play function to resolve all errors
    Modified the audio play function due to the audio not playing and
    throwing an error message instead any time the user clicked on the
    image logo on the webpage. Instead of having the audio element already
    exist in the HTML, I changed the function to have the JS dynamically
    generate the audio element as a child under the logo image and wired up
    the audio file to the generated element. Made some modifications to
    existing DOM elements, removed other DOM elements and commented out the
    existing audio HTML element to get this to work. Also changed the
    parent container of the image from a div to a button in the HTML.
    Changed the image filepath from relative to absolute to reduce JS code.
    It works but there is a slight delay in the audio play due to the file
    itself, NOT the code. Will try to resolve this in the future.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './styles.css';
-import './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
-import './sounds/quick-fart-with-reverb.mp3';
+// import './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
+// import './sounds/quick-fart-with-reverb.mp3';
 
 // Different method of importing the single files above
 
 // import Poop from './img/Microsoft-Fluentui-Emoji-Flat-Pile-Of-Poo-Flat.512.png';
-// import reverbFart from './sounds/quick-fart-with-reverb.mp3';
+import reverbFart from './sounds/quick-fart-with-reverb.mp3';
 
 // Add logo to existing div. Not sure if this should go in the function below, it's own separate function or stay here by itself
 
@@ -18,24 +18,26 @@ import './sounds/quick-fart-with-reverb.mp3';
 // TODO: Uncaught Runtime error of 'The element has no supported sources'. Check src link of audio element in HTML
 // TODO: Need to find a Loader for audio sounds to add to the Webpack config with CSS and image files
 function poopSound() {
-  const poopLogo = document.querySelector("div[data-image=app-logo]"); // Does this need to be outside this function if we implement logo add above?
+  const poopLogo = document.querySelector(".app-logo"); // Does this need to be outside this function if we implement logo add above?
 
   poopLogo.addEventListener('click', function(e) {
-    const audio = document.querySelector("audio[data-image=app-logo]");
+    // Create the audio element via JS
+    const audio = document.createElement("audio");
+    audio.setAttribute("data-image", "logo-sound");
 
-    // Create the audio element via JS as an alternate option?
+    // Wire the mp3 file up to the already existing img file
+    const imageButton = document.querySelector(".image-button");
+    audio.src = reverbFart;
 
-    // const audio = document.createElement("audio");
-    // audio.setAttribute("data-image", "app-logo");
+    // Text to show if the users' browser doesn't support the mp3 file
+    const audioError = document.createTextNode("Your browser does not support the HTML5 audio tag.");
+    audio.appendChild(audioError);
 
-    // Maybe this to add in the audio itself?
-    
-    // const quickFart = new Audio();
-    // quickFart.src = reverbFart;
-    // audio.setAttribute("src", "quickFart")
+    // Append the audio itself to the image file
+    imageButton.appendChild(audio);
 
     if(!audio) return; // stops function from running altogether
-    audio.currentTime = 0; // rewinds sound to the start
+    audio.currentTime = 0; // rewinds sound to the start. Can I make this a 1 or 2 to get it to start later?
     audio.play();
   });
 };
