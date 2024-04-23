7506c7e7f92a9a7fe9ed7cf0780a9109ec5a7dfe
pgodettejr <paul.godette@gmail.com>
2024-04-23 15:24:44 -0400

    Refactor everything that called renamed HTML
    Refactored all DOM elements and other code to new naming convention that
    matches the current naming in the HTML form elements.

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,7 @@
 // TODO: Don't forget to create a container element in the DOM for everything to generate under, then return that container element at the bottom of this file
 
 // Global DOM elements related to adding projects and tasks to the app
-const projectForm = document.getElementById("project-dialog");
+const projectForm = document.getElementById("task-dialog");
 const confirmBtn = document.getElementById("confirmBtn");
 const cancelBtn = document.getElementById("cancelBtn");
 
@@ -9,7 +9,6 @@ const cancelBtn = document.getElementById("cancelBtn");
 const myProjects = [];
 
 // Class for making "Task" objects & reporting the "Task" project
-// TODO: IF we're going the nested object/array route, we need to figure out how to implement it either in this Class or the addTaskToProject function below
 class Task {
   constructor(title, dueDate, time, priority, description, task) {
     this.title = title;
@@ -17,21 +16,21 @@ class Task {
     this.time = time;
     this.priority = priority; // Research how to add colors next to each option in the Drop down menu and how to make this "required" in the <select> element
     this.description = description;
-    this.task = task; // This will be an "Add Task" button so might not even need this. Button will pull up another <dialog> for tasks to save to this
+    this.task = task; // Changing this to "Add to Project" drop-down menu, so might not even need this?
     // this.checked = false; <-- Checkbox whose default is not checked. Does NOT currently add the "checkbox" itself to the task (see HTML file for details)
     // this.id = Date.now(); <-- Id number for each project assigned at "random?"
   }
 }
-// this.task = function addTask() {
+// this.task = function addTaskToClass() {
 //   todo: '';
 // }
 
 // Stores new Tasks objects into the myProjects array via user input. 
 // NOTE: DOM elements may need to be changed to querySelector if they don't work
-function addTaskToProject() {
-  let title = document.getElementById("project-title").value;
+function addTask() {
+  let title = document.getElementById("task-title").value;
   let dueDate = document.getElementById("due-date").value;
-  let time = document.getElementById("project-time").value;
+  let time = document.getElementById("task-time").value;
   let priority = document.getElementById("priority").value;
   let description = document.getElementById("description").value;
   // let task = document.getElementById("").value;
@@ -39,15 +38,15 @@ function addTaskToProject() {
   if (title && dueDate && priority) { // Do we need !isNaN(dueDate) instead?
     const newTask = new Task(title, dueDate, time, priority, description, task)
     myProjects.push(newTask);
-    // projectDisplay(); <-- Function for dynamically adding the project to the <main> area that needs to be written below
+    // taskDisplay(); <-- Function for dynamically adding the task to the <main> area that needs to be written below
   }
 }
 
-function projectDisplay() {
+function taskDisplay() {
   // For in loop goes here
-    // Generate all project/task info here using the "DOM level 1" technique (see "Traversing an HTML table with JS & DOM interfaces" documentation if needed)
+    // Generate all task info here using the "DOM level 1" technique (see "Traversing an HTML table with JS & DOM interfaces" documentation if needed)
     // e.g: bookDisplay() function in the Library project
     // TODO: Also generate the project as a button in the sidebar?
 }
 
-export { projectForm, confirmBtn, cancelBtn, myProjects, Task, addTaskToProject, projectDisplay }
+export { projectForm, confirmBtn, cancelBtn, myProjects, Task, addTask, taskDisplay }
