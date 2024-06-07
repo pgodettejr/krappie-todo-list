d4d458aeacfe7d176967d3785d287515001e6162
pgodettejr <paul.godette@gmail.com>
2024-06-07 09:17:20 -0400

    Remove splice code and comments no longer needed
    Removed all splice methods from all functions as leaving it in would
    produce more potential problems later on. It limits the ways in which
    we can approach updating and deleting projects and tasks since they
    would no longer be in the array (would be heavily UI dependent). Removed
    commented out code and other notes that were no longer needed.

--- src/krappieUI.js
+++ src/krappieUI.js
@@ -71,44 +71,7 @@ function renderProject() {
       });
     });
   }
-
-  appState.myProjects.splice(-1, 1) // TODO: Try to take this out if possible. Don't want to splice out projects if we don't have to.
 }
-
-// Class for making "Task" objects & reporting the "Task" project
-// class Task {
-//   constructor(taskTitle, dueDate, time, priority, description, checked, id) {
-//     this.taskTitle = taskTitle;
-//     this.dueDate = dueDate;
-//     this.time = time; // Changing this to "Add to Project" drop-down menu
-//     this.priority = priority; // TODO: Research how to add colors next to each option in the Drop down menu (img files that are color dots & add them as children in the HTML?)
-//     this.description = description; 
-//     this.checked = false; // Checkbox whose default is not checked. Does NOT currently add the "checkbox" itself to the task (see HTML file for details)
-//     this.id = Date.now(); // Id number for each project assigned at "random?". Would be used to find the task that the user clicked on to delete
-//   }
-// }
-
-// Code example if we had gone with the nested object option (add tasks within the Add Project dialog)
-// this.task = function addTaskToClass() {
-//   todo: '';
-// }
-
-// Stores new Tasks objects into the myProjects array via user input. 
-// NOTE: DOM elements may need to be changed to querySelector if they don't work
-// function addTask() {
-//   let taskTitle = document.getElementById("task-title").value; // Potentially add '.trim()' to the end of this line if there's whitespace that needs to be removed
-//   let dueDate = document.getElementById("due-date").value;
-//   let time = document.getElementById("task-time").value;
-//   let priority = document.getElementById("priority").value;
-//   let description = document.getElementById("description").value;
-//   // let task = document.getElementById("").value;
-
-//   if (taskTitle && dueDate && priority) { // Do we need !isNaN(dueDate) instead?
-//     const newTask = new Task(taskTitle, dueDate, time, priority, description, checked, id);
-//     myProjects.push(newTask); // Possibly change this to 'arr.push(newTask)' to reflect the task being added to the project array that was set up via user input
-//     // taskDisplay(); <-- Function for dynamically adding the task to the <main> area that needs to be written below
-//   }
-// }
 
 // Render all task info here using the "DOM level 1" technique (see "Traversing an HTML table with JS & DOM interfaces" documentation if needed)
 // Function for dynamically adding the task to the <main> area
@@ -159,8 +122,8 @@ function renderTask() {
     taskName.appendChild(taskPriority);
     taskName.appendChild(taskDescription);
 
-    // TODO: Figure out how to append taskName to the <ul> that was made in projects.js
-    projectName.appendChild(taskName); // Don't think it's this simple since projectName is in a separate file
+    // Places the task itself (via it's name) as a child under the Project <ul>
+    projectName.appendChild(taskName);
 
     // TODO: Code for a conditional statement that checks to see if the project the user selected exists before adding the task to it (if else statement)
   }
