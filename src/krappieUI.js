34c9e1536a70722cb298d4fe8be86d7a000e61c5
pgodettejr <paul.godette@gmail.com>
2024-06-10 13:13:37 -0400

    Modify renderProject with conditional statement
    Modified renderProject slightly by placing all code under a conditional
    statement that was supposed to run only if the project in the array
    doesn't match the user input in the project form. This was in an
    attempt to stop the function from duplicating projects in the array
    that were already rendered prior, but it has no effect.
    
    Declared the project's name again under renderTask in order to get the
    Task form to submit the user input info in the UI. It works but none of
    the info put into the form shows in the UI. Instead all text shows as
    'undefined'. The elements themselves do render correctly (in their
    proper places).

--- src/krappieUI.js
+++ src/krappieUI.js
@@ -15,62 +15,65 @@ const cancelTask = document.getElementById("cancelTask");
 // TODO: Currently adds all projects in the array like before (duplicates) now that splice is removed
 function renderProject() {
   for (const project in appState.myProjects) {
-    // DOM for "My Projects" section of the sidebar as well as the main area
-    const projectSidebar = document.querySelector(".menu-2");
-    const main = document.querySelector("main");
+    // This conditional doesn't change anything (still duplicates previous projects). If it's '===' then no project generates at all.
+    if (project !== appState.myProjects[project].projectTitle) {
+      // DOM for "My Projects" section of the sidebar as well as the main area
+      const projectSidebar = document.querySelector(".menu-2");
+      const main = document.querySelector("main");
 
-    // Generate the sidebar button
-    const projectBtn = document.createElement("button");
-    let projectBtnText = document.createTextNode(`${appState.myProjects[project].projectTitle}`);
-    projectBtn.classList.add("project-btn");
+      // Generate the sidebar button
+      const projectBtn = document.createElement("button");
+      let projectBtnText = document.createTextNode(`${appState.myProjects[project].projectTitle}`);
+      projectBtn.classList.add("project-btn");
 
-    // Function to remove all currently showing content in main when a Project button is clicked. 
-    // Just goes back to adding all projects in the array like before (duplicates) if splice is removed
-    // projectBtn.addEventListener('click', () => {
-    //   main.replaceChildren(projectName);
-    // });
+      // Function to remove all currently showing content in main when a Project button is clicked. 
+      // Just goes back to adding all projects in the array like before (duplicates) if splice is removed
+      // projectBtn.addEventListener('click', () => {
+      //   main.replaceChildren(projectName);
+      // });
 
-    // Generate "Project Name" header to be added to the main area
-    const projectName = document.createElement("ul");
-    let projectNameText = document.createTextNode(`${appState.myProjects[project].projectTitle}`);
-    projectName.classList.add("project-name");
+      // Generate "Project Name" header to be added to the main area
+      const projectName = document.createElement("ul");
+      let projectNameText = document.createTextNode(`${appState.myProjects[project].projectTitle}`);
+      projectName.classList.add("project-name");
 
-    projectBtn.appendChild(projectBtnText);
-    projectSidebar.appendChild(projectBtn);
-    projectName.appendChild(projectNameText);
-    main.appendChild(projectName);
+      projectBtn.appendChild(projectBtnText);
+      projectSidebar.appendChild(projectBtn);
+      projectName.appendChild(projectNameText);
+      main.appendChild(projectName);
 
-    // When here under projectDisplay():
+      // When here under renderProject():
 
-    // This function will only replace the current project showing in main area once. Will not show another project afterwards if you click it's related sidebar button
-    // No matter what button is clicked, all previous <ul> are removed and only the last <ul> shows, even if it's not related to the button clicked.
-    // Keep in mind the <ul> remains when projectDisplay is run, but the project related to it inside the array is deleted due to splice
+      // This function will only replace the current project showing in main area once. Will not show another project afterwards if you click it's related sidebar button
+      // No matter what button is clicked, all previous <ul> are removed and only the last <ul> shows, even if it's not related to the button clicked.
+      // Keep in mind the <ul> remains when renderProject is run, but the project related to it inside the array is deleted due to splice
 
-    // When placed in index.js by itself, the buttons do nothing on click
+      // When placed in index.js by itself, the buttons do nothing on click
 
-    // TODO: We need to figure out how to separate this code somehow. projectDisplay() is doing too much as it is
-    const projectButtons = document.querySelectorAll(".project-btn");
+      // TODO: We need to figure out how to separate this code somehow. renderProject() is doing too much as it is
+      const projectButtons = document.querySelectorAll(".project-btn");
 
-    projectButtons.forEach(projectBtn => {
-      projectBtn.addEventListener('click', () => { // Took the "e" out of the parentheses. Will put it back in if needed later on.
-        // TODO: Code to generate the project related to the button clicked via "tabbed browsing" goes here. See example code in comments below.
-        // Goal is to render the page elements of the corresponding button WITHOUT recreating the button again
+      projectButtons.forEach(projectBtn => {
+        projectBtn.addEventListener('click', () => { // Took the "e" out of the parentheses. Will put it back in if needed later on.
+          // TODO: Code to generate the project related to the button clicked via "tabbed browsing" goes here. See example code in comments below.
+          // Goal is to render the page elements of the corresponding button WITHOUT recreating the button again
 
-        // TODO: Debug and step through this function and find out exactly what is happening when previously generated buttons are clicked
-        // NOTE: Do we need to write code that says if the value of the ul is equal to the value of the button, then replace any current content and append the ul of that button
-        main.replaceChildren();
-        main.appendChild(projectName); // currentProject argument gives Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
+          // TODO: Debug and step through this function and find out exactly what is happening when previously generated buttons are clicked
+          // NOTE: Do we need to write code that says if the value of the ul is equal to the value of the button, then replace any current content and append the ul of that button
+          main.replaceChildren();
+          main.appendChild(projectName); // currentProject argument gives Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node'.
 
-        // Previous version that tried to run the code only if the text from the button and the ul matched up
+          // Previous version that tried to run the code only if the text from the button and the ul matched up
 
-        // const currentProject = document.querySelector(".project-name");
+          // const currentProject = document.querySelector(".project-name");
 
-        // if (button.textContent === currentProject.textContent) {
-        //   main.replaceChildren(); 
-        //   main.appendChild(currentProject);
-        // }
+          // if (button.textContent === currentProject.textContent) {
+          //   main.replaceChildren(); 
+          //   main.appendChild(currentProject);
+          // }
+        });
       });
-    });
+    }
   }
 
   // Reinstate this if we can't figure out how to stop duplicates
@@ -104,9 +107,14 @@ function renderTask() {
     // taskList.classList.add("task-info");
 
     // Checkbox rendering goes here or under taskName
+    // TODO: Change this to createTask.checked and see if that will show the "checked" status on render? (currently doesn't show "checked" status at all)
     const isChecked = task.checked ? 'done' : '';
 
+    // DOM for existing "Project Name" header (that was previously rendered)
+    const projectName = document.querySelector("ul");
+
     // Sets up the name of the task entered as a list element so the user can have a list of tasks
+    // TODO: "data-key" is showing as undefined, likely due to 'task.id' being incorrect in showing the ID number for the task created
     const taskName = document.createElement("li");
     taskName.setAttribute('data-key', task.id);
     taskName.classList.add(`todo-item-${isChecked}`);
@@ -121,6 +129,7 @@ function renderTask() {
     taskDescription.classList.add("task-description");
 
     // Text info DOM that takes user input from the "Add Task" form and creates text nodes to be attached to the <p> tags above
+    // TODO: ALL of these are showing as "undefined" in the UI and DevTools (elements themselves are rendering correctly)
     const taskNameInfo = document.createTextNode(`${appState.myProjects[task].taskTitle}`);
     const taskDateInfo = document.createTextNode(`${appState.myProjects[task].dueDate}`);
     const taskPriorityInfo = document.createTextNode(`${appState.myProjects[task].priority}`);
@@ -139,7 +148,7 @@ function renderTask() {
     taskName.appendChild(taskDescription);
 
     // Places the task itself (via it's name) as a child under the Project <ul>
-    // TODO: Uncaught ReferenceError: projectName is not defined - currently preventing this function from finishing
+    // TODO: It works currently. It's just showing undefined on everything is all (not due to this code itself)
     projectName.appendChild(taskName);
 
     // TODO: Code for a conditional statement that checks to see if the project the user selected exists before adding the task to it (if else statement)
